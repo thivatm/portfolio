f51a3c4a001529c09210b383a5a6a37355d8d19c
Thivagar <thivagartm@gmail.com>
2020-05-27 20:47:56 +0530

    Updates

---
+++ precache-manifest.ae41a297df7b3a905d204bbfc5115e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caa66da063beb6109ad2cf54db0fd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49562a951a9e1234dec1",
+    "url": "/static/css/main.8399c9d6.chunk.css"
+  },
+  {
+    "revision": "604410a0f479cc85554f",
+    "url": "/static/js/2.cae5c8d2.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.cae5c8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49562a951a9e1234dec1",
+    "url": "/static/js/main.c456b3de.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
